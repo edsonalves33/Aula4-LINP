d6c37918a10c3aa5dfcd52630527bbf34361ea55
edsonalves33 <edsonalvesdesouza03@gmail.com>
2026-03-18 21:25:10 -0300

    Add files via upload

---
+++ lojac.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   float valor=0;
+   printf ("qual o valor da sua compra ?");
+   scanf("%f", &valor);
+   
+   if(valor>=50){
+    printf("Parabens!!! voce ganhou um premio");
+   }
+}

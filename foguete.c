0bb75e246dc64ca5bd96ccb5bc04c35c38bcdd12
edsonalves33 <edsonalvesdesouza03@gmail.com>
2026-03-28 09:34:00 -0300

    Add files via upload

---
+++ foguete.c
@@ -0,0 +1,17 @@
+foguete.c
+#include <stdio.h>
+
+int main(){
+
+
+   printf("o foguete vai ser lançado em:\n");
+
+
+    int contador = 5;
+    while (contador >=0){
+        printf("%d\n", contador);
+        contador--;
+    }
+    printf("lancar!");
+    return 0;
+    }

26af8cce5d58f7a4919a5be36e3cc999d397853b
edsonalves33 <edsonalvesdesouza03@gmail.com>
2026-03-28 09:25:00 -0300

    Add files via upload

---
+++ atedez.c
@@ -0,0 +1,16 @@
+atedez.c
+#include <stdio.h>
+
+int main(){
+
+    int numero;
+   printf("digite um numero: ");
+   scanf("%d", &numero);
+   
+    int contador = (numero);
+    while (contador <= 10){
+        printf("%d\n", contador = contador );
+        contador++;
+    }
+    return 0;
+    }
